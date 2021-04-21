2add45b0bab304ddd92051b1caa77bc90aae2813
hminiken <33667037+hminiken@users.noreply.github.com>
2021-04-20 17:55:10 -0700

    Updated theme colors

--- lib/classes/theme.dart
+++ lib/classes/theme.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 
-  Color colTeal = Color(0xFF2A9D8F);
   Color colDarkBlue = Color(0xFF264653);
-  Color colYellow = Color(0xFF264653);
-  Color colOrange = Color(0xFF264653);
-  Color colRed = Color(0xFF264653);
+  Color colTeal = Color(0xFF2A9D8F);
+  Color colYellow = Color(0xFFE9C46A);
+  Color colOrange = Color(0xFFF4A261);
+  Color colRed = Color(0xFFE76F51);
 
 final ThemeData CuddlerThemeData = new ThemeData(
   brightness: Brightness.light,
