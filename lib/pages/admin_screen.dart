957f87d2cc781bb63ff9a0a4e5932e221d008fcd
Sariah Bunnell <33461825+sariah9@users.noreply.github.com>
2021-05-26 22:42:16 -0700

    Add files via upload

--- lib/pages/admin_screen.dart
+++ lib/pages/admin_screen.dart
@@ -31,19 +31,47 @@ class _AdminState extends State<AdminPage> {
         ),
         body: TabBarView(
           children: [
-            UsersList(),
-            AnimalsList(
-                type: 'Dog',
-                animals: FirebaseFirestore.instance.collection('dogs')),
-            AnimalsList(
-                type: 'Cat',
-                animals: FirebaseFirestore.instance.collection('cats')),
-            AnimalsList(
-                type: 'Extra',
-                animals: FirebaseFirestore.instance.collection('others')),
+            buildStack('Users', UsersList()),
+            buildStack(
+                'Dog',
+                AnimalsList(
+                    animals: FirebaseFirestore.instance.collection('dogs'))),
+            buildStack(
+                'Cat',
+                AnimalsList(
+                    animals: FirebaseFirestore.instance.collection('cats'))),
+            buildStack(
+                'Extra',
+                AnimalsList(
+                    animals: FirebaseFirestore.instance.collection('others'))),
           ],
         ),
       ),
+    );
+  }
+
+  Widget buildStack(String type, Widget list) {
+    return Stack(
+      fit: StackFit.loose,
+      clipBehavior: Clip.hardEdge,
+      alignment: AlignmentDirectional.topStart,
+      children: <Widget>[
+        ListTile(
+          leading: Icon(Icons.star),
+          title: Text((type + ' Profiles').toUpperCase(),
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              )),
+        ),
+        Positioned(
+          left: 0,
+          right: 0,
+          top: 60,
+          bottom: 0,
+          child: (list),
+        ),
+      ],
     );
   }
 }
@@ -55,76 +83,58 @@ class UsersList extends StatelessWidget {
   Widget build(BuildContext context) {
     //Grab the collection from firebase
     CollectionReference users = FirebaseFirestore.instance.collection('users');
-    bool firstTile = true;
-    var count = 0;
     return StreamBuilder<QuerySnapshot>(
       stream: users.snapshots(),
       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
         if (snapshot.hasData && snapshot.data!.docs.length > 0) {
-          firstTile = true;
           return ListView.builder(
-              itemCount: snapshot.data!.docs.length + 1,
+              itemCount: snapshot.data!.docs.length,
               itemBuilder: (context, index) {
-                if (index == 0 && firstTile == true) {
-                  firstTile = false;
-                  count = 0;
+                var post = snapshot.data!.docs[index];
+                if (post.id == currUser.uid) {
                   return ListTile(
-                    leading: Icon(Icons.star),
-                    title: Text('User Profiles'.toUpperCase(),
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold,
-                        )),
-                  );
-                } else {
-                  var post = snapshot.data!.docs[count];
-                  count += 1;
-                  if (post.id == currUser.uid) {
-                    return ListTile(
-                      leading: Icon(Icons.account_circle_outlined),
-                      title:
-                          Text('Current User', style: TextStyle(fontSize: 18)),
-                      subtitle: Text(
-                          'Account cannot be changed or deleted from this screen'),
-                    );
-                  }
-                  var username = post['fName'];
-                  var email = post['email'];
-                  var pic = post['profileImgURL'];
-                  return ListTile(
-                    leading: CircleAvatar(
-                      radius: 30.0,
-                      backgroundImage: NetworkImage(pic),
-                      backgroundColor: Colors.transparent,
-                    ),
-                    title: Text(
-                      username,
-                      style: TextStyle(fontSize: 18),
-                    ),
-                    subtitle: Text(email),
-                    trailing: Wrap(
-                      spacing: 12,
-                      children: <Widget>[
-                        IconButton(
-                          icon: Icon(Icons.edit),
-                          color: Constants.deepBlue,
-                          onPressed: () {
-                            //navigate to update_user_profile
-                            users.doc(post.id).update({'fName': 'Bob'});
-                          },
-                        ),
-                        IconButton(
-                          icon: Icon(Icons.delete),
-                          color: Constants.redOrange,
-                          onPressed: () {
-                            //should we also un-authenticate?
-                            users.doc(post.id).delete();
-                          },
-                        ),
-                      ],
-                    ),
+                    leading: Icon(Icons.account_circle_outlined),
+                    title: Text('Current User', style: TextStyle(fontSize: 18)),
+                    subtitle: Text(
+                        'Account cannot be changed or deleted from this screen'),
                   );
                 }
+                var username = post['fName'];
+                var email = post['email'];
+                var pic = post['profileImgURL'];
+                return ListTile(
+                  leading: CircleAvatar(
+                    radius: 30.0,
+                    backgroundImage: NetworkImage(pic),
+                    backgroundColor: Colors.transparent,
+                  ),
+                  title: Text(
+                    username,
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  subtitle: Text(email),
+                  trailing: Wrap(
+                    spacing: 12,
+                    children: <Widget>[
+                      IconButton(
+                        icon: Icon(Icons.edit),
+                        color: Constants.deepBlue,
+                        onPressed: () {
+                          //navigate to update_user_profile
+                          users.doc(post.id).update({'fName': 'Bob'});
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.delete),
+                        color: Constants.redOrange,
+                        onPressed: () {
+                          //should we also un-authenticate?
+                          users.doc(post.id).delete();
+                        },
+                      ),
+                    ],
+                  ),
+                );
               });
         } else {
           return Center(child: CircularProgressIndicator());
@@ -136,9 +146,7 @@ class UsersList extends StatelessWidget {
 
 //Widget for displaying list of Animal Profiles
 class AnimalsList extends StatefulWidget {
-  AnimalsList({Key? key, required this.type, required this.animals})
-      : super(key: key);
-  final String type;
+  AnimalsList({Key? key, required this.animals}) : super(key: key);
   final CollectionReference animals;
 
   @override
@@ -148,68 +156,51 @@ class AnimalsList extends StatefulWidget {
 class _AnimalState extends State<AnimalsList> {
   @override
   Widget build(BuildContext context) {
-    bool firstTile = true;
-    var count = 0;
     return StreamBuilder<QuerySnapshot>(
       stream: widget.animals.snapshots(),
       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
         if (snapshot.hasData && snapshot.data!.docs.length > 0) {
-          firstTile = true;
           return ListView.builder(
-              itemCount: snapshot.data!.docs.length + 1,
+              itemCount: snapshot.data!.docs.length,
               itemBuilder: (context, index) {
-                if (index == 0 && firstTile == true) {
-                  firstTile = false;
-                  count = 0;
-                  return ListTile(
-                    leading: Icon(Icons.star),
-                    title: Text((widget.type + ' Profiles').toUpperCase(),
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold,
-                        )),
-                  );
-                } else {
-                  var profile = snapshot.data!.docs[count];
-                  count += 1;
-                  var age = profile['age'].toString();
-                  var sex = profile['sex'];
-                  var pic = profile['url'];
-                  return ListTile(
-                    leading: CircleAvatar(
-                      radius: 30.0,
-                      backgroundImage: NetworkImage(pic),
-                      backgroundColor: Colors.transparent,
-                    ),
-                    title: Text(
-                      profile['name'],
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    subtitle: Text('Age: $age Sex: $sex'),
-                    trailing: Wrap(
-                      spacing: 12,
-                      children: <Widget>[
-                        IconButton(
-                          icon: Icon(Icons.edit),
-                          color: Constants.deepBlue,
-                          onPressed: () {
-                            //navigate to update_animal_profile
-                            widget.animals
-                                .doc(profile.id)
-                                .update({'name': 'Bob'});
-                          },
-                        ),
-                        IconButton(
-                          icon: Icon(Icons.delete),
-                          color: Constants.redOrange,
-                          onPressed: () {
-                            widget.animals.doc(profile.id).delete();
-                          },
-                        ),
-                      ],
-                    ),
-                  );
-                }
+                var profile = snapshot.data!.docs[index];
+                var age = profile['age'].toString();
+                var sex = profile['sex'];
+                var pic = profile['url'];
+                return ListTile(
+                  leading: CircleAvatar(
+                    radius: 30.0,
+                    backgroundImage: NetworkImage(pic),
+                    backgroundColor: Colors.transparent,
+                  ),
+                  title: Text(
+                    profile['name'],
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  subtitle: Text('Age: $age Sex: $sex'),
+                  trailing: Wrap(
+                    spacing: 12,
+                    children: <Widget>[
+                      IconButton(
+                        icon: Icon(Icons.edit),
+                        color: Constants.deepBlue,
+                        onPressed: () {
+                          //navigate to update_animal_profile
+                          widget.animals
+                              .doc(profile.id)
+                              .update({'name': 'Bob'});
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.delete),
+                        color: Constants.redOrange,
+                        onPressed: () {
+                          widget.animals.doc(profile.id).delete();
+                        },
+                      ),
+                    ],
+                  ),
+                );
               });
         } else {
           return Center(child: CircularProgressIndicator());
